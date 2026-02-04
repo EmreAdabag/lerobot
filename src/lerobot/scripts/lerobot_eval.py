2fd98a67a569e7e7fe24e7ec47df07ce40a47f6b
ea <emreadabag@gmail.com>
2026-02-04 16:51:51 -0500

    mixed precision eval crash fix

--- src/lerobot/scripts/lerobot_eval.py
+++ src/lerobot/scripts/lerobot_eval.py
@@ -182,7 +182,7 @@ def rollout(
         action = action_transition[ACTION]
 
         # Convert to CPU / numpy.
-        action_numpy: np.ndarray = action.to("cpu").numpy()
+        action_numpy: np.ndarray = action.to(device="cpu", dtype=torch.float32).numpy()
         assert action_numpy.ndim == 2, "Action dimensions should be (batch, action_dim)"
 
         # Apply the next action.
